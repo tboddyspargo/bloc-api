84f45496b6dc59aad54c1e58ff06b61c5ec62f42
Tyler BoddySpargo <muyleche@gmail.com>
2018-01-04 04:50:19 +0000

    checkpoint 4 - mentor availability: complete

--- lib/kele.rb
+++ lib/kele.rb
@@ -4,21 +4,25 @@ require 'pry'
 
 class Kele
 	include HTTParty
+	base_uri 'https://www.bloc.io/api/v1'
 	
 	def initialize(email, password)
-		@bloc_url = 'https://www.bloc.io/api/v1'
-		response = self.class.post 'https://www.bloc.io/api/v1/sessions', body: {email: email, password: password}
-		response_data = parse_response(response)
-		@auth_token = response_data['auth_token']
-		@user = response_data['user']
+		response = self.class.post "/sessions", body: {email: email, password: password}
+		@auth_token = parse_response(response)['auth_token']
 	end
 	
 	def get_me
-		if !@user
-			response = self.class.get 'https://www.bloc.io/api/v1/users/me', headers: { authorization: @auth_token }
-			@user = parse_response(response)
-		end
-		@user
+		response = self.class.get "/users/me", headers: { authorization: @auth_token }
+		parse_response(response)
+	end
+	
+	# @function
+	# @description This function will return the availability of a student's mentor.
+	# @param {int} mentor_id The id of the mentor whose availability you want to check.
+	# @return {hash}  
+	def get_mentor_availability(mentor_id=get_me['current_enrollment']['mentor_id'])
+		response = self.class.get "/mentors/#{mentor_id}/student_availability", headers: { authorization: @auth_token }
+		parse_response(response)
 	end
 	
 	private
