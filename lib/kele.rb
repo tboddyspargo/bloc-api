a23b4c7c7f998571c32396c68e63eb3bb51bb96e
Tyler BoddySpargo <muyleche@gmail.com>
2018-01-04 05:00:51 +0000

    add comments and remove pry

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,31 +1,43 @@
 require 'httparty'
 require 'json'
-require 'pry'
 
 class Kele
 	include HTTParty
 	base_uri 'https://www.bloc.io/api/v1'
 	
+	# @function
+	# @description							This function will instantiate a Kele object with an authentication token for the Bloc API.
+	# @param {String} email 		The email of the user you wish to authenticate as.
+	# @param {String} password	The password of the user you wish to authenticate as.
+	# @return {Array<Hash>}  
 	def initialize(email, password)
 		response = self.class.post "/sessions", body: {email: email, password: password}
 		@auth_token = parse_response(response)['auth_token']
 	end
 	
+	# @function
+	# @description			This function will return the data associated with the current user.
+	# @return {Hash}		
 	def get_me
 		response = self.class.get "/users/me", headers: { authorization: @auth_token }
 		parse_response(response)
 	end
 	
 	# @function
-	# @description This function will return the availability of a student's mentor.
-	# @param {int} mentor_id The id of the mentor whose availability you want to check.
-	# @return {hash}  
+	# @description								This function will return the availability of a student's mentor.
+	# @param {Integer} mentor_id	The id of the mentor whose availability you want to check.
+	# @return {Array<Hash>} 			
 	def get_mentor_availability(mentor_id=get_me['current_enrollment']['mentor_id'])
 		response = self.class.get "/mentors/#{mentor_id}/student_availability", headers: { authorization: @auth_token }
 		parse_response(response)
 	end
 	
 	private
+	
+	# @function
+	# @description												A private function to handle HTTP requests that return JSON data.
+	# @param {HTTPartyResponse} response	The response object returned by the HTTParty gem's HTTP requests.
+	# @return {Hash|Array}								A hash or array containing the body of the HTTP response (if successful).
 	def parse_response(response)
 		body = JSON.parse response.body
 		if response.code == 200
