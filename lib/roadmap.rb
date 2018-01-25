d110741f1b3fafa837f785c19bcc299850cb2ef9
Tyler BoddySpargo <muyleche@gmail.com>
2018-01-25 00:19:44 +0000

    checkpoint 8 - extra credit: complete

--- lib/roadmap.rb
+++ lib/roadmap.rb
@@ -20,15 +20,15 @@ module Roadmap
 	end
 	
 	# @function																								create_submission
-	# @description																						This function will submit a checkpoint and assignment
+	# @description																						This function will submit a checkpoint assignment
 	# @param					{Integer}				checkpoint_id						The ID of the checkpoint you wish to submit too.
 	# @param					{String}				assignment_branch				The name of the assignment branch you want to submit.		
-	# @param					{String}				assignment_commit_link	The commit identifier.
-	# @param					{String}				comment									The comment you wish to attach to this Bloc submission.
-	def create_submission(checkpoint_id, assignment_branch, assignment_commit_link, comment="")
+	# @param					{String}				assignment_commit_url		The URL to the github commit for this assignment submission.
+	# @param					{String}				comment									The comment you wish to attach to this Bloc assignment submission.
+	def create_submission(checkpoint_id, assignment_branch, assignment_commit_url, comment="")
 		enrollment_id = Helpers.get_attribute(get_me, 'enrollment')['id']
 		options = { assignment_branch: assignment_branch,
-								assignment_commit_link: assignment_commit_link, 
+								assignment_commit_link: assignment_commit_url, 
 								checkpoint_id: checkpoint_id, 
 								comment: comment, 
 								enrollment_id: enrollment_id }
@@ -38,22 +38,20 @@ module Roadmap
 	end
 	
 	# @function																								create_submission
-	# @description																						This function will submit a checkpoint and assignment
-	# @param					{Integer}				checkpoint_id						The ID of the checkpoint you wish to submit too.
+	# @description																						This function will update an assignment submission and associate it with a checkpoint.
+	# @param					{Integer}				id											The ID of the assignment submission you wish to modify.
+	# @param					{Integer}				checkpoint_id						The ID of the checkpoint this assignment submission should be associated with.
 	# @param					{String}				assignment_branch				The name of the assignment branch you want to submit.		
-	# @param					{String}				assignment_commit_link	The commit identifier.
-	# @param					{String}				comment									The comment you wish to attach to this Bloc submission.
-	def update_submission(id, checkpoint_id, assignment_branch, assignment_commit_link, comment="")
+	# @param					{String}				assignment_commit_url		The URL to the github commit for this assignment submission.
+	# @param					{String}				comment									The comment you wish to attach to this Bloc assignment submission.
+	def update_submission(id, checkpoint_id, assignment_branch, assignment_commit_url, comment="")
 		enrollment_id = Helpers.get_attribute(get_me, 'enrollment')['id']
-		options = { id: id,
-								assignment_branch: assignment_branch,
-								assignment_commit_link: assignment_commit_link, 
+		options = { assignment_branch: assignment_branch,
+								assignment_commit_link: assignment_commit_url, 
 								checkpoint_id: checkpoint_id, 
 								comment: comment, 
 								enrollment_id: enrollment_id }
-		response = self.class.post "/checkpoint_submissions/#{checkpoint_id}", body: options, headers: { authorization: @auth_token }
+		response = self.class.put "/checkpoint_submissions/#{id}", body: options, headers: { authorization: @auth_token }
 		Helpers.parse_response(response)
 	end
-	
-	
 end
